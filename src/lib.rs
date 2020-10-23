1309b6d40990bbabf9a89e55b1134e2a7f2d1470
Lukas Simon <lukas.b.simon@gmail.com>
2020-10-23 22:28:53 +0200

    Simple bloom filter with insert and check
    Co-authored-by: Helge Wrede <helge.wrede@protonmail.com>
    Co-authored-by: Alexander Schultheiß <alexanderschultheiss@pm.me>

---
+++ src/lib.rs
@@ -0,0 +1,80 @@
+use std::hash::{Hash, Hasher};
+
+// capacity
+// probability false positive
+// number of hash functions
+
+pub trait ResettableHasher: Hasher {
+    fn reset(&mut self);
+}
+
+impl<T> ResettableHasher for T
+where
+    T: Default + Hasher,
+{
+    fn reset(&mut self) {
+        *self = Self::default();
+    }
+}
+
+pub struct BloomFilter {
+    hashers: Vec<Box<dyn ResettableHasher>>,
+    hits: Vec<bool>,
+    capacity: usize,
+    count: usize,
+}
+
+impl BloomFilter {
+    pub fn new(capacity: usize, hashers: Vec<Box<dyn ResettableHasher>>) -> Self {
+        Self {
+            hits: vec![false; capacity * hashers.len()],
+            hashers,
+            capacity,
+            count: 0,
+        }
+    }
+}
+
+impl BloomFilter {
+    pub fn insert<T>(&mut self, data: T)
+    where
+        T: Hash,
+    {
+        for (i, hasher) in self.hashers.iter_mut().enumerate() {
+            hasher.reset();
+            data.hash(hasher);
+            self.hits[BloomFilter::index(i, self.capacity, hasher.finish())] = true;
+        }
+
+        self.count += 1;
+    }
+
+    pub fn check<T>(&mut self, data: &T) -> bool
+    where
+        T: Hash,
+    {
+        for (i, hasher) in self.hashers.iter_mut().enumerate() {
+            data.hash(hasher);
+            if !self.hits[BloomFilter::index(i, self.capacity, hasher.finish())] {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    pub fn false_positive_probability(&self) -> f64 {
+        (1.0 - std::f64::consts::E
+            .powf(-(self.hashers.len() as f64) * self.count as f64 / self.capacity as f64))
+        .powf(self.capacity as f64)
+    }
+
+    fn index(i: usize, capacity: usize, hash: u64) -> usize {
+        i * capacity + hash as usize % capacity
+    }
+}
+
+impl Default for BloomFilter {
+    fn default() -> Self {
+        todo!()
+    }
+}
