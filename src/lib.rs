7c9b3a98298dfec2253603350ba83b85bb290a6b
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2020-10-24 23:40:07 +0200

    Added first doc test

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,28 @@ mod seeded_bloom_filter;
 pub use km_bloom_filter::KMBloomFilter;
 pub use seeded_bloom_filter::SeededBloomFilter;
 
+/**
+ A default implementation of BloomFilter using ahash::AHasher and collections::hash_map::DefaultHasher.
+
+ DefaultBloomFilter is implemented as a type definition `type DefaultBloomFilter = BloomFilter<ahash::AHasher, DefaultHasher>;`
+ # Examples
+ ```
+ use bloom_filter::DefaultBloomFilter;
+
+ fn simple_bloom_filter_test() {
+     let desired_capacity = 1_000_000;
+     let false_positive_probability = 0.0001;
+     let mut bloom_filter = DefaultBloomFilter::new(desired_capacity, false_positive_probability);
+
+     bloom_filter.insert(&"Hello!");
+     bloom_filter.insert(&34);
+
+     assert!(bloom_filter.check(&"Hello!"));
+     assert!(bloom_filter.check(&34));
+     assert_eq!(false, bloom_filter.check(&"Not in filter"));
+ }
+ ```
+*/
 pub type DefaultBloomFilter = KMBloomFilter<ahash::AHasher, DefaultHasher>;
 
 pub trait BloomFilter {
