cb1c1cb1a51abfd9376eae7689c89c1fa7cb6f79
Lukas Simon <lukas.b.simon@gmail.com>
2021-03-24 11:15:34 +0100

    Replaced phantom data with hasher members, moved functions into separate impl blocks

--- src/km_bloom_filter.rs
+++ src/km_bloom_filter.rs
@@ -1,7 +1,6 @@
 use std::{
     fmt::Debug,
     hash::{Hash, Hasher},
-    marker::PhantomData,
 };
 
 use crate::{
@@ -44,20 +43,218 @@ use crate::{
 /// ```
 pub struct KMBloomFilter<H1, H2>
 where
-    H1: Hasher + Default,
-    H2: Hasher + Default,
+    H1: Hasher + Clone,
+    H2: Hasher + Clone,
 {
     number_of_hashers: usize,
     bitset: Bitset,
     bits_per_hasher: usize,
-    // Phantom data for saving which concrete Hasher types are used
-    _phantom: PhantomData<(H1, H2)>,
+    hasher1: H1,
+    hasher2: H2,
 }
 
 impl<H1, H2> KMBloomFilter<H1, H2>
 where
-    H1: Hasher + Default,
-    H2: Hasher + Default,
+    H1: Hasher + Clone,
+    H2: Hasher + Clone,
+{
+    /// Approximate number of elements stored.
+    /// Approximation technique taken from Wikipedia:
+    /// > Wikipedia, ["Bloom filter"](https://en.wikipedia.org/wiki/Bloom_filter#Approximating_the_number_of_items_in_a_Bloom_filter) [Accessed: 02.12.2020]
+    pub fn approximate_element_count(&self) -> f64 {
+        approximate_element_count(
+            self.number_of_hashers,
+            self.bits_per_hasher,
+            self.bitset.count_ones(),
+        )
+    }
+
+    /// Return the current approximate false positive probability which depends on the current
+    /// number of elements in the filter.
+    ///
+    /// The probability is given as a value in the interval [0,1]
+    /// Approximation technique taken from Sagi Kedmi:
+    /// > S. Kedmi, ["Bloom Filters for the Perplexed"](https://sagi.io/bloom-filters-for-the-perplexed/), July 2017 [Accessed: 02.12.2020]
+    pub fn approximate_current_false_positive_probability(&self) -> f64 {
+        approximate_false_positive_probability(
+            self.number_of_hashers,
+            self.bits_per_hasher,
+            self.approximate_element_count(),
+        )
+    }
+
+    /// Checks whether two bloom filters were created with the same desired capacity and desired false
+    /// positive probability.
+    pub fn eq_configuration(&self, other: &Self) -> bool {
+        self.number_of_hashers == other.number_of_hashers
+            && self.bits_per_hasher == other.bits_per_hasher
+    }
+
+    fn index(i: usize, bits_per_hash: usize, hash_a: u64, hash_b: u64) -> usize {
+        i * bits_per_hash
+            + hash_a.wrapping_add((i as u64).wrapping_mul(hash_b)) as usize % bits_per_hash
+    }
+}
+impl<H1, H2> KMBloomFilter<H1, H2>
+where
+    H1: Hasher + Clone,
+    H2: Hasher + Clone,
+{
+    /// Creates a union of this bloom filter and 'other', which means 'contains' of the resulting
+    /// bloom filter will always return true for elements inserted in either this bloom filter or in
+    /// 'other' before creation.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the desired capacity or desired false positive probability of 'self' and 'other'
+    /// differ.
+    ///
+    /// # Examples
+    ///
+    /// Union of two bloom filters with the same configuration.
+    /// ```
+    /// use bloom_filter_simple::{BloomFilter,KMBloomFilter};
+    /// use ahash::AHasher;
+    /// use std::collections::hash_map::DefaultHasher;
+    ///
+    /// fn main() {
+    ///     // The configuration of both bloom filters has to be the same
+    ///     let desired_capacity = 10_000;
+    ///     let desired_fp_probability = 0.0001;
+    ///
+    ///     // We initialize two new KMBloomFilter
+    ///     let mut filter_one: KMBloomFilter<AHasher, DefaultHasher> = KMBloomFilter::new(
+    ///         desired_capacity,
+    ///         desired_fp_probability
+    ///     );
+    ///
+    ///     let mut filter_two: KMBloomFilter<AHasher, DefaultHasher> = KMBloomFilter::new(
+    ///         desired_capacity,
+    ///         desired_fp_probability
+    ///     );
+    ///
+    ///     // Insert elements into the first filter
+    ///     filter_one.insert(&0);
+    ///     filter_one.insert(&1);
+    ///
+    ///     // Insert elements into the second filter
+    ///     filter_two.insert(&2);
+    ///     filter_two.insert(&3);
+    ///     
+    ///     // Now we retrieve the union of both filters
+    ///     let filter_union = filter_one.union(&filter_two);
+    ///
+    ///     // The union will return true for a 'contains' check for the elements inserted
+    ///     // previously into at least one of the constituent filters.
+    ///     assert_eq!(true, filter_union.contains(&0));
+    ///     assert_eq!(true, filter_union.contains(&1));
+    ///     assert_eq!(true, filter_union.contains(&2));
+    ///     assert_eq!(true, filter_union.contains(&3));
+    /// }
+    /// ```
+    pub fn union(&self, other: &Self) -> Self {
+        if !self.eq_configuration(other) {
+            panic!("unable to union k-m bloom filters with different configurations");
+        }
+        Self {
+            number_of_hashers: self.number_of_hashers,
+            bitset: self.bitset.union(&other.bitset),
+            bits_per_hasher: self.bits_per_hasher,
+            hasher1: self.hasher1.clone(),
+            hasher2: self.hasher2.clone(),
+        }
+    }
+
+    /// Creates a intersection of this bloom filter and 'other', which means 'contains' of the resulting
+    /// bloom filter will always return true for elements inserted both in this bloom filter and in
+    /// 'other' before creation.
+    /// The false positive probability of the resulting bloom filter is at most the false positive
+    /// probability of 'other' or 'self'.
+    /// The false positive probability of the resulting bloom filter may be bigger than the false
+    /// positive probability of a new empty bloom filter with the intersecting elements inserted.
+    /// The functions 'approximate_current_false_positive_probability' and 'approximate_element_count'
+    /// called on the resulting bloom filter may return too big approximations.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the desired capacity or desired false positive probability of 'self' and 'other'
+    /// differ.
+    ///
+    /// # Examples
+    ///
+    /// Intersection of two bloom filters with the same configuration.
+    /// ```
+    /// use bloom_filter_simple::{BloomFilter,KMBloomFilter};
+    /// use ahash::AHasher;
+    /// use std::collections::hash_map::DefaultHasher;
+    ///
+    /// fn main() {
+    ///     // The configuration of both bloom filters has to be the same
+    ///     let desired_capacity = 10_000;
+    ///     let desired_fp_probability = 0.0001;
+    ///
+    ///     // We initialize two new KMBloomFilter
+    ///     let mut filter_one: KMBloomFilter<AHasher, DefaultHasher> = KMBloomFilter::new(
+    ///         desired_capacity,
+    ///         desired_fp_probability
+    ///     );
+    ///
+    ///     let mut filter_two: KMBloomFilter<AHasher, DefaultHasher> = KMBloomFilter::new(
+    ///         desired_capacity,
+    ///         desired_fp_probability
+    ///     );
+    ///
+    ///     // Insert elements into the first filter
+    ///     filter_one.insert(&0);
+    ///     filter_one.insert(&1);
+    ///
+    ///     // Insert elements into the second filter
+    ///     filter_two.insert(&1);
+    ///     filter_two.insert(&2);
+    ///     
+    ///     // Now we retrieve the intersection of both filters
+    ///     let filter_intersection = filter_one.intersect(&filter_two);
+    ///
+    ///     // The intersection will return true for a 'contains' check for the elements inserted
+    ///     // previously into both constituent filters.
+    ///     assert_eq!(false, filter_intersection.contains(&0));
+    ///     assert_eq!(true, filter_intersection.contains(&1));
+    ///     assert_eq!(false, filter_intersection.contains(&2));
+    /// }
+    /// ```
+    pub fn intersect(&self, other: &Self) -> Self {
+        if !self.eq_configuration(other) {
+            panic!("unable to intersect k-m bloom filters with different configurations");
+        }
+        Self {
+            number_of_hashers: self.number_of_hashers,
+            bitset: self.bitset.intersect(&other.bitset),
+            bits_per_hasher: self.bits_per_hasher,
+            hasher1: self.hasher1.clone(),
+            hasher2: self.hasher2.clone(),
+        }
+    }
+
+    fn generate_hashes<T>(&self, data: &T) -> (u64, u64)
+    where
+        T: Hash,
+    {
+        let mut hasher = self.hasher1.clone();
+        data.hash(&mut hasher);
+        let hash_a = hasher.finish();
+
+        let mut hasher = self.hasher2.clone();
+        data.hash(&mut hasher);
+        let hash_b = hasher.finish();
+
+        (hash_a, hash_b)
+    }
+}
+
+impl<H1, H2> KMBloomFilter<H1, H2>
+where
+    H1: Hasher + Default + Clone,
+    H2: Hasher + Default + Clone,
 {
     /// Initialize a new instance of KMBloomFilter that guarantees that the false positive rate
     /// is less than *desired_false_positive_probability* for up to *desired_capacity*
@@ -100,200 +297,16 @@ where
             bitset: Bitset::new(bits_per_hasher * number_of_hashers),
             number_of_hashers,
             bits_per_hasher,
-            _phantom: PhantomData,
+            hasher1: H1::default(),
+            hasher2: H2::default(),
         }
-    }
-
-    /// Approximate number of elements stored.
-    /// Approximation technique taken from Wikipedia:
-    /// > Wikipedia, ["Bloom filter"](https://en.wikipedia.org/wiki/Bloom_filter#Approximating_the_number_of_items_in_a_Bloom_filter) [Accessed: 02.12.2020]
-    pub fn approximate_element_count(&self) -> f64 {
-        approximate_element_count(
-            self.number_of_hashers,
-            self.bits_per_hasher,
-            self.bitset.count_ones(),
-        )
-    }
-
-    /// Return the current approximate false positive probability which depends on the current
-    /// number of elements in the filter.
-    ///
-    /// The probability is given as a value in the interval [0,1]
-    /// Approximation technique taken from Sagi Kedmi:
-    /// > S. Kedmi, ["Bloom Filters for the Perplexed"](https://sagi.io/bloom-filters-for-the-perplexed/), July 2017 [Accessed: 02.12.2020]
-    pub fn approximate_current_false_positive_probability(&self) -> f64 {
-        approximate_false_positive_probability(
-            self.number_of_hashers,
-            self.bits_per_hasher,
-            self.approximate_element_count(),
-        )
-    }
-
-    /// Creates a union of this bloom filter and 'other', which means 'contains' of the resulting
-    /// bloom filter will always return true for elements inserted in either this bloom filter or in
-    /// 'other' before creation.
-    ///
-    /// # Panics
-    ///
-    /// Panics if the desired capacity or desired false positive probability of 'self' and 'other'
-    /// differ.
-    /// 
-    /// # Examples
-    /// 
-    /// Union of two bloom filters with the same configuration.
-    /// ```
-    /// use bloom_filter_simple::{BloomFilter,KMBloomFilter};
-    /// use ahash::AHasher;
-    /// use std::collections::hash_map::DefaultHasher;
-    ///
-    /// fn main() {
-    ///     // The configuration of both bloom filters has to be the same
-    ///     let desired_capacity = 10_000;
-    ///     let desired_fp_probability = 0.0001;
-    ///
-    ///     // We initialize two new KMBloomFilter 
-    ///     let mut filter_one: KMBloomFilter<AHasher, DefaultHasher> = KMBloomFilter::new(
-    ///         desired_capacity,
-    ///         desired_fp_probability
-    ///     );
-    /// 
-    ///     let mut filter_two: KMBloomFilter<AHasher, DefaultHasher> = KMBloomFilter::new(
-    ///         desired_capacity,
-    ///         desired_fp_probability
-    ///     );
-    /// 
-    ///     // Insert elements into the first filter
-    ///     filter_one.insert(&0);
-    ///     filter_one.insert(&1);
-    /// 
-    ///     // Insert elements into the second filter
-    ///     filter_two.insert(&2);
-    ///     filter_two.insert(&3);
-    ///     
-    ///     // Now we retrieve the union of both filters
-    ///     let filter_union = filter_one.union(&filter_two);
-    ///
-    ///     // The union will return true for a 'contains' check for the elements inserted 
-    ///     // previously into at least one of the constituent filters.
-    ///     assert_eq!(true, filter_union.contains(&0));
-    ///     assert_eq!(true, filter_union.contains(&1));
-    ///     assert_eq!(true, filter_union.contains(&2));
-    ///     assert_eq!(true, filter_union.contains(&3));
-    /// }
-    /// ```
-    pub fn union(&self, other: &Self) -> Self {
-        if !self.eq_configuration(other) {
-            panic!("unable to union k-m bloom filters with different configurations");
-        }
-        Self {
-            number_of_hashers: self.number_of_hashers,
-            bitset: self.bitset.union(&other.bitset),
-            bits_per_hasher: self.bits_per_hasher,
-            _phantom: self._phantom,
-        }
-    }
-
-    /// Creates a intersection of this bloom filter and 'other', which means 'contains' of the resulting
-    /// bloom filter will always return true for elements inserted both in this bloom filter and in
-    /// 'other' before creation.
-    /// The false positive probability of the resulting bloom filter is at most the false positive
-    /// probability of 'other' or 'self'.
-    /// The false positive probability of the resulting bloom filter may be bigger than the false
-    /// positive probability of a new empty bloom filter with the intersecting elements inserted.
-    /// The functions 'approximate_current_false_positive_probability' and 'approximate_element_count'
-    /// called on the resulting bloom filter may return too big approximations.
-    ///
-    /// # Panics
-    ///
-    /// Panics if the desired capacity or desired false positive probability of 'self' and 'other'
-    /// differ.
-    /// 
-    /// # Examples
-    /// 
-    /// Intersection of two bloom filters with the same configuration.
-    /// ```
-    /// use bloom_filter_simple::{BloomFilter,KMBloomFilter};
-    /// use ahash::AHasher;
-    /// use std::collections::hash_map::DefaultHasher;
-    ///
-    /// fn main() {
-    ///     // The configuration of both bloom filters has to be the same
-    ///     let desired_capacity = 10_000;
-    ///     let desired_fp_probability = 0.0001;
-    ///
-    ///     // We initialize two new KMBloomFilter 
-    ///     let mut filter_one: KMBloomFilter<AHasher, DefaultHasher> = KMBloomFilter::new(
-    ///         desired_capacity,
-    ///         desired_fp_probability
-    ///     );
-    /// 
-    ///     let mut filter_two: KMBloomFilter<AHasher, DefaultHasher> = KMBloomFilter::new(
-    ///         desired_capacity,
-    ///         desired_fp_probability
-    ///     );
-    /// 
-    ///     // Insert elements into the first filter
-    ///     filter_one.insert(&0);
-    ///     filter_one.insert(&1);
-    /// 
-    ///     // Insert elements into the second filter
-    ///     filter_two.insert(&1);
-    ///     filter_two.insert(&2);
-    ///     
-    ///     // Now we retrieve the intersection of both filters
-    ///     let filter_intersection = filter_one.intersect(&filter_two);
-    ///
-    ///     // The intersection will return true for a 'contains' check for the elements inserted 
-    ///     // previously into both constituent filters.
-    ///     assert_eq!(false, filter_intersection.contains(&0));
-    ///     assert_eq!(true, filter_intersection.contains(&1));
-    ///     assert_eq!(false, filter_intersection.contains(&2));
-    /// }
-    /// ```
-    pub fn intersect(&self, other: &Self) -> Self {
-        if !self.eq_configuration(other) {
-            panic!("unable to intersect k-m bloom filters with different configurations");
-        }
-        Self {
-            number_of_hashers: self.number_of_hashers,
-            bitset: self.bitset.intersect(&other.bitset),
-            bits_per_hasher: self.bits_per_hasher,
-            _phantom: self._phantom,
-        }
-    }
-
-    /// Checks whether two bloom filters were created with the same desired capacity and desired false
-    /// positive probability.
-    pub fn eq_configuration(&self, other: &Self) -> bool {
-        self.number_of_hashers == other.number_of_hashers
-            && self.bits_per_hasher == other.bits_per_hasher
-    }
-
-    fn generate_hashes<T>(&self, data: &T) -> (u64, u64)
-    where
-        T: Hash,
-    {
-        let mut hasher = H1::default();
-        data.hash(&mut hasher);
-        let hash_a = hasher.finish();
-
-        let mut hasher = H2::default();
-        data.hash(&mut hasher);
-        let hash_b = hasher.finish();
-
-        (hash_a, hash_b)
-    }
-
-    fn index(i: usize, bits_per_hash: usize, hash_a: u64, hash_b: u64) -> usize {
-        i * bits_per_hash
-            + hash_a.wrapping_add((i as u64).wrapping_mul(hash_b)) as usize % bits_per_hash
     }
 }
 
 impl<H1, H2> Debug for KMBloomFilter<H1, H2>
 where
-    H1: Hasher + Default,
-    H2: Hasher + Default,
+    H1: Hasher + Clone,
+    H2: Hasher + Clone,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "KMBloomFilter{{{:?}}}", self.bitset)
@@ -302,8 +315,8 @@ where
 
 impl<H1, H2> BloomFilter for KMBloomFilter<H1, H2>
 where
-    H1: Hasher + Default,
-    H2: Hasher + Default,
+    H1: Hasher + Clone,
+    H2: Hasher + Clone,
 {
     fn insert<T>(&mut self, data: &T)
     where
